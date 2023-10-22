6f7c594086b1649e31638ab638b567c9488b6395
Cem Yurtsever <ccemyurtsever+github@gmail.com>
2023-10-22 15:12:47 +0300

    add basic database

--- database.py
+++ database.py
@@ -1,5 +1,27 @@
 import sqlite3 as sql
+import time
 
-def createDatabase():
-    pass
-    
+def dbConnect():
+    global connect
+    connect = sql.connect("database.db")
+    global cursor
+    cursor = connect.cursor()
+
+def dbOut():
+    connect.commit()
+    connect.close()
+    time.sleep(0.5)
+
+
+def create():
+    dbConnect()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS USERS(
+        id integer PRIMARY KEY NOT NULL,
+        name varchar(75) NOT NULL,
+        lastname varchar(75) NOT NULL,
+        username text NOT NULL,
+        password text NOT NULL
+    )""")
+    dbOut()
+
+create()
